8724115823e456289214b7696b45f7c3e38ac34f
Saurav Parajuli <sauravinmail@gmail.com>
2020-11-09 10:14:13 +0545

    Factorial

---
+++ src/factorial.kt
@@ -0,0 +1,13 @@
+fun main(){
+    print("Enter a Number")
+    val a = readLine()!!.toInt()
+    factorial(a=a)
+}
+
+fun factorial(a: Int){
+    var fact = 1
+    for(i in 1..a){
+        fact *= i
+    }
+    print("Factorial of $a is $fact")
+}
